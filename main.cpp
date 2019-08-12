f02588528d26b24b0bc0d3b5f53043dbc3a8f156
Amirhossein <amir78729@aut.ac.ir>
2019-08-12 23:56:27 +0430

    adding a prompt for user

--- main.cpp
+++ main.cpp
@@ -2,27 +2,75 @@
 using namespace std;
 struct user{
     string name;
-    int sign_in_date;
-    int number_of_travels;
-    struct travel *user_travels;
-    struct user* prev_user;
-    struct user* next_user;
+    int sign_in_date = 0;
+    int number_of_travels = 0;
+    struct travel* user_travels = NULL;
+    struct user* prev_user = NULL;
+    struct user* next_user = NULL;
 }user;
 
 struct travel{
     string origin;
     string destination;
-    int money;
-    struct travel* prev_travel;
-    struct travel* next_travel;
+    int money = 0;
+    struct travel* prev_travel = NULL;
+    struct travel* next_travel = NULL;
 }travel;
 
 //function proto-types:
 void add_user();
 void add_travel();
 void remove_user();
+void show_prompt();
+void print_a_user_info();
+void print_a_travel_info();
 
 int main() {
-
+    int command;
+    while(true){
+        show_prompt();
+        cin >> command;
+        //defining a sentinel for our loop
+        if (command == -1){
+            cout << "end of the program";
+            break;
+        }
+        switch (command){
+            case 1:
+                add_user();
+                break;
+            case 2:
+                add_travel();
+                break;
+            case 3:
+                remove_user();
+                break;
+            default:
+                cout << "bad input!" << endl;
+                break;
+        }
+    }
     return 0;
+}
+void show_prompt(){
+    cout << "Enter a command:" << endl;
+    cout << " 1) add a user\n"
+            " 2) add a travel\n"
+            " 3) remove a user\n"
+            "-1) end of the program\n";
+}
+void add_user(){
+    cout << "adding a user..." << endl;
+}
+void add_travel(){
+    cout << "adding a travel..." << endl;
+}
+void remove_user(){
+    cout << "removing a user..." << endl;
+}
+void print_a_user_info(){
+
+}
+void print_a_travel_info(){
+
 }
