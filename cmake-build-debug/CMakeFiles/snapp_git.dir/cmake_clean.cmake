ed7ec90d75042958aeef32b8b8406463eddd8d5c
Amirhossein <amir78729@aut.ac.ir>
2019-08-12 19:27:25 +0430

    creating USER and TRAVEL structures

---
+++ cmake-build-debug/CMakeFiles/snapp_git.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snapp_git.dir/main.cpp.obj"
+  "snapp_git.pdb"
+  "snapp_git.exe"
+  "snapp_git.exe.manifest"
+  "libsnapp_git.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snapp_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+struct user{
+    string name;
+    int sign_in_date;
+    struct user* prev_user;
+    struct user* next_user;
+};
+
+struct travel{
+    string origin;
+    string destination;
+    int money;
+    struct travel* prev_travel;
+    struct travel* next_travel;
+};
+
+int main() {
+
+    return 0;
+}
