997c40855ada2965dcb9346892a649014d2d75a9
Amirhossein <amir78729@aut.ac.ir>
2019-08-12 20:16:39 +0430

    - adding "number_of_travels" as a field inside "user" struct - some function proto-types added

--- main.cpp
+++ main.cpp
@@ -3,10 +3,11 @@ using namespace std;
 struct user{
     string name;
     int sign_in_date;
+    int number_of_travels;
     struct travel *user_travels;
     struct user* prev_user;
     struct user* next_user;
-};
+}user;
 
 struct travel{
     string origin;
@@ -14,7 +15,12 @@ struct travel{
     int money;
     struct travel* prev_travel;
     struct travel* next_travel;
-};
+}travel;
+
+//function proto-types:
+void add_user();
+void add_travel();
+void remove_user();
 
 int main() {
 
